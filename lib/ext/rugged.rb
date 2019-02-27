91d462e488d5661e3fd60e27fdc2f7b919bfead4
Peter Vandenberk <pvandenberk@mac.com>
2019-02-27 10:29:36 +0000

    Ensure correct time-ordering of git references
    For a "regular" commit, the two timestamps in question are the same:
    
    ```
    {:message=>"initial commit\n",
     :committer=>
      {:name=>"Peter Vandenberk",
       :email=>"peter.vandenberk@simplybusiness.co.uk",
       :time=>2019-02-27 10:01:14 +0000},
     :author=>
      {:name=>"Peter Vandenberk",
       :email=>"peter.vandenberk@simplybusiness.co.uk",
       :time=>2019-02-27 10:01:14 +0000},
     :tree=> ... ,
     :parents=>[]}
     ```
    
     ... but for a rebased commit, for instance, the "committer" timestamp
     is later than the "author" timestamp:
    
     ```
     {:message=>"Fireant, the new TrRMIte!\n",
     :committer=>
      {:name=>"user.email",
       :email=>"peter.vandenberk@simplybusiness.co.uk",
       :time=>2019-02-20 13:48:32 +0000},
     :author=>
      {:name=>"user.email",
       :email=>"peter.vandenberk@simplybusiness.co.uk",
       :time=>2019-02-20 11:15:42 +0000},
     :tree=> ... ,
     :parents=>[]}
     ```

--- lib/ext/rugged.rb
+++ lib/ext/rugged.rb
@@ -5,7 +5,19 @@ module Rugged
     end
 
     def time
-      target.time
+      #
+      # we can't just use `target.time` here, because that
+      # corresponds to the time attribute for the commit's
+      # so-called "committer", not for its "author"...
+      #
+      # for regular commits these two time attributes will be
+      # the same, but after rebasing for instance "committer"
+      # will be different from "author", as will their "time"
+      # attributes
+      #
+      # see also: https://git.io/fhNEv
+      #
+      target.author[:time]
     end
 
     def targets?(reference)
